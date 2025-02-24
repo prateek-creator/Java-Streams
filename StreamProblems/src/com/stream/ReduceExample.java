b403474c3ba2e80080160f5f198f6392584768c0
ParteekPa <prateek.modiwal.hrd65@gmail.com>
2025-02-24 16:10:57 +0530

    Reduce example fixed

--- StreamProblems/src/com/stream/ReduceExample.java
+++ StreamProblems/src/com/stream/ReduceExample.java
@@ -7,6 +7,6 @@ public class ReduceExample {
     public static void main(String[] args) {
         List<Integer> numbers= Arrays.asList(1,2,3,4,5,6,7,8,9,10,12,0,19);
 
-        numbers.stream().reduce()
+        int sum=numbers.stream().reduce(0,(a,b)->a+b);
     }
 }
